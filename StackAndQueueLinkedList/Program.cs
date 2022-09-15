a6cbe8091735482b732d680a1894428ad645ffc6
LokeshKiran <lokeshkskiran@gmail.com>
2022-09-15 22:45:44 +0530

    [Lokesh]Added:Ability to create new stack

---
+++ StackAndQueueLinkedList/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueueLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to Stack and Queue Programs");
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.Push(70);
+            linkedListStack.Push(30);
+            linkedListStack.Push(56);
+            linkedListStack.Display();
+        }
+    }
+}
